7bd8bff2870c617bff44e212e17552406b2e9871
Janjan Medina <medinajuanantonio95@gmail.com>
2018-08-13 13:26:21 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# Fore Template Engine
+
+Fore Template Engine is designed to remove code repetition and planned to make revisions faster and easier by using Fore Components and Global or Local Variables.
+
+## Sample Fore Code
+```
+<title text="@title" /> //Title for the page
+
+<imports> // CSS, Javascript Imports
+    <link rel="stylesheet" href="index.css" />
+    <script src="./ezss/js/ezss.js"></script>
+</imports>
+
+<styles> // Use can also use styles
+	.body {
+		width: 100%;
+		margin: 0;
+		padding: 0;
+	}
+</styles>
+
+<content>
+    <div id="main-container">
+        <Header /> //Custom Component Import
+        <About /> //Custom Component Import
+        <div class="nav-right">
+            <a href="#home" class="nav-tab">@home</a>
+            <a href="#about-me" class="nav-tab">@about-me</a>
+            <a href="#skills" class="nav-tab">@skills</a>
+            <a href="#projects" class="nav-tab">@projects</a>
+            <a href="#contact" class="nav-tab">@contact</a>
+        </div>
+    </div>
+</content>
+```
+
+## To start project, run command on your local repository:
+```
+fore init
+```
+
+## File Structure
+- Project
+    - html
+    - css
+    - js
+    - assets
+- Components
+- Fore
+- .gitignore
+- strings.json
+- package.json
+- config.json
+
+## Usage
+
+### Commands
+
+**IMPORTANT: Commands only run on root folder!**
+
+```
+fore init //Initializes Fore Project
+fore compile //Compile Fore files
+fore watch //Runs Fore Watcher to auto hot-reload
+```
+
+### Files
+
+#### /Components/Header.fore
+Header.fore is **NOT** initialized during `fore init`. Fore Components are created manually and **SHOULD** be stored in `/Components` folder. Storing Components to seperated folders will not work and would create a `<ComponentNotFound />` line on file Output.
+
+Sample Header.fore
+```
+<nav class="ezss-nav nav-clear nav">
+    <div class="nav-center">
+        <a href="#home" class="nav-tab">HOME</a>
+        <a href="#about-me" class="nav-tab">ABOUT ME</a>
+        <a href="#skills" class="nav-tab">SKILLS</a>
+        <a href="#projects" class="nav-tab">MY PROJECTS</a>
+        <a href="#contact" class="nav-tab">CONTACT ME</a>
+    </div>
+</nav>
+<nav class="ezss-nav nav-clear nav-right sidebar">
+    <a href="#home" onclick="openNav()" class="nav-tab">&#9776;</a>
+</nav>
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="#home" class="nav-tab">HOME</a>
+    <a href="#about-me" class="nav-tab">ABOUT ME</a>
+    <a href="#skills" class="nav-tab">SKILLS</a>
+    <a href="#projects" class="nav-tab">MY PROJECTS</a>
+    <a href="#contact" class="nav-tab">CONTACT ME</a>
+</div>
+```
+
+#### /Fore/index.fore
+index.fore is **NOT** initialized during `fore init`.
+Fore files stored in `/Fore` directory are the structure files that will be used in generating `.html` files.
+
+#### /Project
+Project folder will be initialized during `fore init`.
+This folder will be used to store fore compiled files and once finished, the only folder needed to be deployed. Using Fore Watcher, saving files will automatically update the `/Project/html` folder files. Fore Watcher does **NOT** allow manual updates of **HTML** files. To compile manually update HTML Files, run `fore compile` in the command line.
+
+#### strings.json
+Strings.json will be used to store Global and Local Variables that can be used in Fore files.
+
+To Create Local Variables, create an object using the filename as the key. Creating different key name would not work.
+
+Sample strings.json Code
+```
+{
+    "title": "My Project", //Global Variables
+    "home": "HOME",
+    "about-me": "ABOUT ME",
+    "skills": "SKILLS",
+    "project": "MY PROJECTS",
+    "contact": "CONTACT US",
+    "index": { //Local Variable Group
+        "title": "Home Page",
+        "con-one": "Lorem Ipsum",
+        "con-two": "Lorem Ipsum",
+        "con-three": "Lorem Ipsum",
+        "css": {
+            "nav-class": "ezss-btn nav-right btn-clear"
+        }
+    },
+    "about-us": {
+        "title": "About Page",
+        "con-one": "Lorem Ipsum",
+        "con-two": "Lorem Ipsum",
+        "con-three": "Lorem Ipsum"
+    }
+}
+```
+
+ To call variables, simply add `@key-name` to your Fore file. In calling local variables, you won't need to call the filename group. In this case, calling `@title` in `index.fore` will return the value of `Home Page`. For accessing inner groups such as the `css` object, calling `css.nav-class` would work fine. If a variable is not found in a Local Variable Group, Fore will automatically get the values from Global Variables.
---
+++ fore.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strings"
+	"regexp"
+	"log"
+	"time"
+	
+	"github.com/radovskyb/watcher"
+	"github.com/tidwall/gjson"
+)
+
+/****** fore home ******/
+
+func home() {
+	fmt.Println("Fore v1.0.0")
+}
+
+/****** fore compile ******/
+
+func compile() {
+	//Get Files of current directory
+	files, _ := ioutil.ReadDir(pwd() + "/Fore")
+			
+	//Loop to each file in current directory
+	for _, f := range files {
+		if filetypefunc(f.Name()) == "fore" {
+			title, imports, styles, scripts, content, genhtmlfile := readfile(pwd(), f.Name())
+
+			htmlcompile(title, imports, styles, scripts, content, genhtmlfile)
+		}
+	}
+}
+
+/****** fore init ******/
+
+func initrun() {
+	//Generate folders
+	gendir("Project")
+	gendir("Components")
+	gendir("Fore")
+
+	//Generate Subfolders
+	gendir("Project/html")
+	gendir("Project/css")
+	gendir("Project/js")
+	gendir("Project/assets")
+
+	//Generate strings.json for global 
+	//and reusable variable strings
+	genfile("strings.json", "{\n\t\"title\": \"My Project\"\n}")
+	genfile("package.json", "")
+	genfile("config.json", "")
+	genfile(".gitignore", "/Components\n/Fore\n*.json")
+
+	fmt.Println("Fore Project Initialized.")
+}
+
+func watch() {
+	w := watcher.New()
+	w.SetMaxEvents(1)
+
+	fmt.Println("Fore Watcher Started")
+	
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:	
+				//Kasi nagcoconflict yung compile(), nababasa as new event
+				//So bawal na imanual update yung html.
+				//To change html files, manual compile needed
+				if filetypefunc(event.Path) != "html" {
+					compile()
+					fmt.Println(event.Path + " saved. -", time.Now().Format(time.RFC850))
+				}
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				fmt.Println("Fore Watcher Stopped")
+			}
+		}
+	}()
+	if err := w.AddRecursive("."); err != nil {
+		log.Fatalln(err)
+	}
+
+	go func() {
+		w.Wait()
+	}()
+
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+/****** Filename and Filetype ******/
+
+func filenamefunc(filename string) string {
+	output := ""
+	split := strings.Split(filename, ".")
+
+	for a := 0; a < len(split)-1; a++ {
+		if output == "" {
+			output = output + split[a] 
+		} else {
+			output = output + "." + split[a] 
+		}
+	}
+
+	return output
+}
+
+func filetypefunc(filename string) string {
+	split := strings.Split(filename, ".")
+
+	return split[len(split)-1]
+}
+
+/****** File Manipulation ******/
+
+func gettabs(line string) string {
+	//Gets the tabs to add before a string
+	match, _ := regexp.Compile("[\\s\t]*")
+    return match.FindString(line)
+}
+
+func checktitle(line string) string {
+	//Check if file line is a Title Component
+	match, _ := regexp.Compile("<title[\\s]*text=\"([A-Za-z0-9\\s]*)\"[\\s]*/>")
+    
+	if(len(match.FindStringSubmatch(line)) > 0) {
+		return match.FindStringSubmatch(line)[1]
+	} else {
+		return match.FindString(line)
+	}
+}
+
+func checkcomponent(line string) string {
+	//Check if file line has a Custom Component
+	match, _ := regexp.Compile("<([A-Z][A-Za-z0-9\\-\\_]*)[\\s]*/>")
+
+	if(len(match.FindStringSubmatch(line)) > 0) {
+		return match.FindStringSubmatch(line)[1]
+	} else {
+		return match.FindString(line)
+	}
+}
+
+func checkimports(line string) string {
+	//Check if file line is a start or finish of an import statement
+	match, _ := regexp.Compile("<[/]*imports>")
+    return match.FindString(line)
+}
+
+func checkstyles(line string) string {
+	//Check if file line is a start or finish of styles
+	match, _ := regexp.Compile("<[/]*style>")
+    return match.FindString(line)
+}
+
+func checkscripts(line string) string {
+	//Check if file line is a start or finish of an import statement
+	match, _ := regexp.Compile("<[/]*script>")
+    return match.FindString(line)
+}
+
+func checkcontent(line string) string {
+	//Check if file line is a start or finish of styles
+	match, _ := regexp.Compile("<[/]*content>")
+    return match.FindString(line)
+}
+
+func checkvariables(line string) []string {
+	//Check if file line has a variable
+	match, _ := regexp.Compile("@([A-Za-z0-9\\-\\_]*)")
+
+	if(len(match.FindAllString(line, -1)) > 0) {
+		return match.FindAllString(line, -1)
+	} else {
+		return make([]string, 0)
+	}
+}
+
+/****** Get JSON Strings ******/
+
+func getjson(filename string) string {
+	dat, _ := ioutil.ReadFile(filename)
+
+	r, _ := regexp.Compile("[\n\t]")
+	return r.ReplaceAllString(string(dat), "")
+}
+
+/****** Get Current Working Directory ******/
+
+func pwd() string{
+	//Gets current working directory
+	dir, _ := os.Getwd()
+	
+	//returns working directory
+	return dir
+}
+
+/****** Generate Directory ******/
+
+func gendir(dirname string) {
+	//choose your permissions well
+	pathErr := os.Mkdir(dirname, 0777)
+ 
+	//check if you need to panic, fallback or report
+	if pathErr != nil {
+		fmt.Println(pathErr)
+	}
+}
+
+/****** Generate File ******/
+
+func genfile(filename string, content string) {
+	file, _ := os.Create(filename)
+    defer file.Close()
+	fmt.Fprintf(file, content)
+}
+
+/****** Read Fore File ******/
+
+func readfile(pwd string, filename string) (string, string, string, string, string, string){
+	title, imports, styles, scripts, content := "", "", "", "", ""	
+	importcheck, stylecheck, scriptcheck, contentcheck := 0, 0, 0, 0
+
+	genhtmlfile := "Project/html/" + filenamefunc(filename) + ".html"
+
+	dat, _ := ioutil.ReadFile(pwd + "/Fore/" + filename)
+
+	splitfile := strings.Split(string(dat), "\n")
+
+	stringsfile := getjson("strings.json")
+	
+	for _, line := range splitfile {
+		vars := checkvariables(line)
+	
+		for _, tempvars := range vars {
+			//This removes the @ symbol of the variable
+			newvars := strings.Replace(tempvars, "@", "", -1)
+
+			// Checks if variable name (strings) has an equivalent
+			// Key in the JSON File (stringsfile)
+			valueGlobal := gjson.Get(stringsfile, newvars)
+			valueLocal := gjson.Get(stringsfile, filenamefunc(filename) + "." + newvars)
+
+			//So if value is not null (meaning it has an equivalent)
+			if valueLocal.Str != "" {
+				//It switches the variable (strings) to the key value (value)
+				line = strings.Replace(line, tempvars, valueLocal.Str, -1)
+			} else if valueGlobal.Str != "" {
+				line = strings.Replace(line, tempvars, valueGlobal.Str, -1)
+			}
+		}
+
+		if importcheck == 0 && stylecheck == 0 && scriptcheck == 0 && contentcheck == 0 {
+			switch {
+				case checkimports(line) != "":
+					importcheck = 1
+				case checkstyles(line) != "":
+					stylecheck = 1
+				case checkscripts(line) != "":
+					scriptcheck = 1
+				case checkcontent(line) != "":
+					contentcheck = 1
+				case checktitle(line) != "":
+					title = checktitle(line)
+			}
+		} else if importcheck == 1 {
+			switch {
+				case checkimports(line) != "":
+					importcheck = 0
+				default: 
+					imports = imports + line + "\n"
+			}
+		} else if stylecheck == 1 {
+			switch {
+				case checkstyles(line) != "":
+					stylecheck = 0
+				default: 
+					styles = styles + line + "\n"
+			}
+		} else if scriptcheck == 1 {
+			switch {
+				case checkscripts(line) != "":
+					scriptcheck = 0
+				default: 
+					scripts = scripts + line + "\n"
+			}
+		} else if contentcheck == 1 {
+			switch {
+				case checkcontent(line) != "":
+					contentcheck = 0
+				case checkcomponent(line) != "":
+					dat, err := ioutil.ReadFile(pwd + "/Components/" + checkcomponent(line) + ".fore")
+					if err != nil {
+						content = content + gettabs(line) + "<ComponentNotFound />"
+					}
+		
+					splitcomp := strings.Split(string(dat), "\n")
+		
+					for _, compline := range splitcomp {
+						content = content + gettabs(line) + compline + "\n"
+					}
+				default: 
+					content = content + line + "\n"
+			}
+		}
+	}
+
+	return title, imports, styles, scripts, content, genhtmlfile
+}
+
+func htmlcompile(title string, imports string, styles string, scripts string, content string, genhtmlfile string) {
+	i, s, sc, c := "", "", "", ""
+
+	importsplit := strings.Split(imports, "\n")
+	for _, line := range importsplit {
+		i = i + "\t" + line + "\n"
+	}
+
+	stylessplit := strings.Split(styles, "\n")
+	for _, line := range stylessplit {
+		s = s + "\t" + line + "\n"
+	}
+
+	scriptsplit := strings.Split(scripts, "\n")
+	for _, line := range scriptsplit {
+		sc = sc + "\t" + line + "\n"
+	}
+
+	contentsplit := strings.Split(content, "\n")
+	for _, line := range contentsplit {
+		c = c + "\t" + line + "\n"
+	}
+
+	html := "<html>\n\t<head>\n"
+	html = html + "\t\t<title>" + title + "</title>\n"
+
+	if imports != "" { html = html + i }
+	if styles != "" { html = html + "\t<style>\n" + s + "\t</style>\n" }
+
+	html = html + "\t</head>\n\t<body>\n"
+
+	if scripts != "" { html = html + "\t\t<script>\n" + sc + "\t\t</script>\n"}
+	if content != "" { html = html + c }
+
+	html = html + "\t</body>\n</html>"
+	
+	genfile(genhtmlfile, html)
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+			case "init":
+				initrun()
+			case "compile", "comp":
+				compile()
+			case "watch", "watcher":
+				watch()
+			case "version":
+				fmt.Println("Fore v1.0.0")
+			default: 
+				fmt.Println("Fore: Command not found.")
+		}
+	} else {
+		home()
+	}
+}
